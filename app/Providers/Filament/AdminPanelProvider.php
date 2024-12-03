b2ea485aa2c322567d8afae67e263ed269958cc5
Shane Rosenthal <srosenthal82@gmail.com>
2024-12-03 09:59:34 -0500

    updates user info

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('/')
             ->login()
+            ->passwordReset()
             ->colors([
                 'primary' => Color::Emerald,
             ])
