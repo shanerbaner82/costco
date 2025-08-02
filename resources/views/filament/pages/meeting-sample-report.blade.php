b8d86bfcc3f52e37573abee812bc00a33ef4de51
Shane Rosenthal <srosenthal82@gmail.com>
2025-08-02 11:15:53 -0600

    u

--- resources/views/filament/pages/meeting-sample-report.blade.php
+++ resources/views/filament/pages/meeting-sample-report.blade.php
@@ -4,15 +4,20 @@
 
         @forelse ($meetings as $meeting)
             <div class="mb-6 border-b pb-4">
-                <div class="font-semibold">Meeting on {{ $meeting->start_time?->format('F j, Y') }} (Status: {{ $meeting->status }})</div>
+                <div class="font-semibold">
+                    @if($meeting->start_time)
+                        Meeting on {{ $meeting->start_time->format('F j, Y') }} (Status: {{ $meeting->status }})
+                    @endif
+                </div>
 
-                    @foreach ($meeting->vendors as $vendor)
-                        <div class="border border-gray-300 rounded mb-10 p-4 mb-6">
-                            @if (! empty($vendor->pivot->samples))
-                                <strong class="text-xl">{{ $vendor->name }}</strong> <br/><br/>{!! $vendor->pivot->samples !!}
-                            @endif
-                        </div>
-                    @endforeach
+                @foreach ($meeting->vendors as $vendor)
+                    <div class="border border-gray-300 rounded mb-10 p-4 mb-6">
+                        @if (! empty($vendor->pivot->samples))
+                            <strong class="text-xl">{{ $vendor->name }}</strong> <br/>
+                            <br/>{!! $vendor->pivot->samples !!}
+                        @endif
+                    </div>
+                @endforeach
             </div>
         @empty
             <p>No meetings found.</p>
